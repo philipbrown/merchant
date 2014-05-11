46cf009e98dd0d4534e58d1f066e6c0f055902bf
philipbrown <phil@ipbrown.com>
2014-05-11 11:54:11 +0100

    Add methods to RegionInterface

--- src/RegionInterface.php
+++ src/RegionInterface.php
@@ -1,3 +1,33 @@
 <?php namespace PhilipBrown\Merchant;
 
-interface RegionInterface {}
+interface RegionInterface {
+
+  /**
+   * Get Name
+   *
+   * @return string
+   */
+  protected function getNameParameter();
+
+  /**
+   * Get Currency
+   *
+   * @return string
+   */
+  protected function getCurrencyParameter();
+
+  /**
+   * Get Tax
+   *
+   * @return boolean
+   */
+  protected function getTaxParameter();
+
+  /**
+   * Get Tax Rate
+   *
+   * @return integer
+   */
+  protected function getTaxRateParameter();
+
+}
