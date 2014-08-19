3424633d8c11781bd22ac54ff3a83e60c58d1a3c
philipbrown <phil@ipbrown.com>
2014-08-19 20:10:41 +0100

    First commit of v2.0

--- src/Exception/InvalidOrderException.php
+++
@@ -1,5 +0,0 @@
-<?php namespace PhilipBrown\Merchant\Exception;
-
-use Exception;
-
-class InvalidOrderException extends Exception {}
--- src/Exception/InvalidRegionException.php
+++
@@ -1,5 +0,0 @@
-<?php namespace PhilipBrown\Merchant\Exception;
-
-use Exception;
-
-class InvalidRegionException extends Exception {}
