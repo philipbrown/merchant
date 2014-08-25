318812d2e0980df32f5850d691900633442a3691
philipbrown <phil@ipbrown.com>
2014-08-25 19:18:26 +0100

    Put stubs into directory and namespace

---
+++ stubs/StubListener.php
@@ -0,0 +1,22 @@
+<?php namespace PhilipBrown\Merchant\Stubs;
+
+use PhilipBrown\Merchant\Product;
+use PhilipBrown\Merchant\Listener;
+use PhilipBrown\Merchant\Collection;
+
+class StubListener implements Listener {
+
+  /**
+   * Hand the event
+   *
+   * @param Product $product
+   * @param Collection $list
+   * @return void
+   */
+  public function handle(Product $product, Collection $list)
+  {
+    $this->product = $product;
+    $this->list = $list;
+  }
+
+}
