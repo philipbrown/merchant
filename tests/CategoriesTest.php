f3c2b8d60445c9cfe4362bfc8e4df9ab226c5ff4
philipbrown <phil@ipbrown.com>
2014-08-25 20:07:18 +0100

    Clean up CategoriesTest

--- tests/CategoriesTest.php
+++ tests/CategoriesTest.php
@@ -3,6 +3,7 @@
 use Money\Money;
 use Money\Currency;
 use PhilipBrown\Merchant\Product;
+use PhilipBrown\Merchant\Stubs\StubTaxRate;
 use PhilipBrown\Merchant\Categories\PhysicalBook;
 
 class CategoriesTest extends PHPUnit_Framework_TestCase {
@@ -10,12 +11,11 @@ class CategoriesTest extends PHPUnit_Framework_TestCase {
   /** @test */
   public function should_categorise_as_physicalbook()
   {
-    $product = new Product(
-      '123',
-      'The 4 Hour Work Week',
-      new Money(1000, new Currency('GBP')),
-      new StubTaxRate
-    );
+    $sku      = '123';
+    $name     = 'The 4 Hour Work Week';
+    $price    = new Money(1000, new Currency('GBP'));
+    $rate     = new StubTaxRate;
+    $product  = new Product($sku, $name, $price, $rate);
 
     $category = new PhysicalBook;
     $category->categorise($product);
